09d39debcdc2ccfbab2846471296666afa72c3f2
Pijsuh Mohanto <pijushmohanto@gmail.com>
2021-03-29 23:25:18 +0600

    Password Reset

--- src/main/java/com/myweb/bookswap/dao/PasswordTokenRepository.java
+++ src/main/java/com/myweb/bookswap/dao/PasswordTokenRepository.java
@@ -1,5 +1,6 @@
 package com.myweb.bookswap.dao;
 
+import com.myweb.bookswap.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.myweb.bookswap.entity.PasswordResetToken;
@@ -7,5 +8,7 @@ import com.myweb.bookswap.entity.PasswordResetToken;
 public interface PasswordTokenRepository extends JpaRepository<PasswordResetToken, Long>  {
 
 	PasswordResetToken findBytoken(String token);
+	PasswordResetToken findByuser(User user);
+
 
 }
